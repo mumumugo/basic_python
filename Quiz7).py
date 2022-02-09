ec0f55414982fabe7be128bea6798e01ae254c03
mumumugo <beapilotga@gmail.com>
2022-02-09 17:36:14 -0500

    2/9

---
+++ Quiz7).py
@@ -0,0 +1,9 @@
+#Quiz) 1 report per week. The format as below:
+#Format:
+#- X week) Weekly Report
+#Department:
+#Name:
+#Summary:
+
+#write a code that can make 1 week to 50 week's report file.
+# file name: '1w.txt', '2w.txt','3w.txt',...

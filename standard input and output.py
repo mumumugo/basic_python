f3d5a585e280a5e0d529c0ae3d4bd4bcaef4ac07
mumumugo <beapilotga@gmail.com>
2022-02-09 15:59:01 -0500

    new items

--- standard input and output.py
+++ standard input and output.py
@@ -1,13 +1,18 @@
 print("python", "java", "c++", sep=" vs ")
 print("python", "java", "c++", sep=" vs ", end="?")
-
-#---------
+print(" ")
+print("------------------------------------------------")
 import sys
 print ("python","java", file=sys.stdout)  #표준 출력
 print ("python","java", file=sys.stderr) #따로 에러 처리
 
-#---------
+print("------------------------------------------------")
 scores = {"math":0,"English":50, "Coding":  100}
 for subject, score in scores.items():
     print(subject, score)
     #print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+print("------------------------------------------------")
+#wait line number
+for num in range(1,6):
+    print("Waiting List: "  + str(num).zfill(3)) #fill out number like 001,002,003...

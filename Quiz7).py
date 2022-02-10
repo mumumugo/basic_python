3b9c102650cc9e9da887a0778203a3f3c94d1963
mumumugo <beapilotga@gmail.com>
2022-02-10 17:10:32 -0500

    2/10

--- Quiz7).py
+++ Quiz7).py
@@ -1,4 +1,4 @@
-#Quiz) 1 report per week. The format as below:
+#Quiz) one report per week. The format as below:
 #Format:
 #- X week) Weekly Report
 #Department:
@@ -7,3 +7,10 @@
 
 #write a code that can make 1 week to 50 week's report file.
 # file name: '1w.txt', '2w.txt','3w.txt',...
+
+for week in range(1,3):
+    with open(str(week) + "weekly_report.txt","w", encoding="utf8") as report_file:
+        report_file.write("-----{0} week) Weekly Report----". format(week))
+        report_file.write("\nDepartment:")
+        report_file.write("\nName:")
+        report_file.write("\nSummary:")
